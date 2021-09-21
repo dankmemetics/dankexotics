af77f94590885cccea03b4bc8761d0b7bf9bfcf5
Dank Memetics <dankmemetics@yahoo.com>
2021-09-21 19:20:58 -0400

    Updates

--- _next/static/DV_svm3HiR6ntFV5XpCmh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-e4e4f47a713459bf1413.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
