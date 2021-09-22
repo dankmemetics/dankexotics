e66e8d4a86d9ceb274d8f8828812e59ba280b169
Dank Memetics <dankmemetics@yahoo.com>
2021-09-22 12:58:26 -0400

    Updates

---
+++ _next/static/Gd9N-OAMeLDeMH_NSIXTs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-95d7a77850b9af7b2d5f.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
